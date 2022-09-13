b6448a15075cc91d3c8c6ea1fc227e89ee8726a9
Eduardo Almeida <eduardofralmeida745@gmail.com>
2022-09-13 11:36:00 -0300

    modulo-3

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './app/src/js/app.js',
@@ -8,10 +9,21 @@ module.exports = {
         path: path.resolve(__dirname,'app/dist'),
         clean: true
     },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader','css-loader' ] }
+        ]
+    },
     plugins: [new HtmlWebpackPlugin ({
         template: './app/src/app.html',
         filename: 'app.html',
         hash: true
-    })]
+    })
+    // new CopyWebpackPlugin ({
+    //     patterns: [
+    //         {from: './app/src/css', to: 'css'}
+    //     ]
+    // })
+]
 
 };
